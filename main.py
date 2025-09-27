834b56914c83ef6eb50b93ca4263c07f0e83cbc0
un4gt <mt3085570450@outlook.com>
2025-09-27 21:54:35 +0800

    initial commit

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
+from astrbot.core.config.astrbot_config import AstrBotConfig
 
-@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
-class MyPlugin(Star):
-    def __init__(self, context: Context):
+@register("astrbot_plugin_v2ex", "un4gt", "通过 astrbot 浏览 v2ex 网站", "1.0.0")
+class V2ex(Star):
+    def __init__(self, context: Context, config: AstrBotConfig):
         super().__init__(context)
+        self._personal_access_token = config.get("personal_access_token")
 
     async def initialize(self):
-        """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
-    
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
+        if self._personal_access_token is None:
+            logger.error("未配置个人访问令牌，请在配置文件中添加 personal_access_token 字段")
+
+
+    @filter.command("v2ex")
     async def helloworld(self, event: AstrMessageEvent):
         """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
         user_name = event.get_sender_name()
@@ -19,6 +22,3 @@ class MyPlugin(Star):
         message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
         logger.info(message_chain)
         yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
-
-    async def terminate(self):
-        """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
